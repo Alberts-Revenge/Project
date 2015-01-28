affad68926ccb9e1cdb956d20ab2ff1fbaf10518
A00770012 <A00770012bcit@gmail.com>
2015-01-28 15:52:44 -0800

    Configure the static page to codeIgniter Signed-off-by:A00770012 <A00770012bcit@gmail.com>

---
+++ application/controllers/Jedihunt.php
@@ -0,0 +1,20 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Jedihunt extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('jedihunt');
+	}
+}
+
+/* End of file Jedihunt.php */
+/* Location: ./application/controllers/Jedihunt.php */
---
+++ application/controllers/Torturer.php
@@ -0,0 +1,20 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Torturer extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('torturer');
+	}
+}
+
+/* End of file Torturer.php */
+/* Location: ./application/controllers/Torturer.php */
