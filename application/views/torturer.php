9172355b22ccc60fc6f0ac59127226838258a76b
A00770012 <A00770012bcit@gmail.com>
2015-03-01 15:43:48 -0800

    fixed the image url location and tab url 	for each tab url Signed-off-by:A00770012 <A00770012bcit@gmail.com>

--- application/views/torturer.php
+++ application/views/torturer.php
@@ -1,14 +1,14 @@
 
+<div>
     <div>
-        <div>
-            <div class="row">
-                {planets}
-                <div class="span4">
-                    <h2>{planet}</h2>
-                    <img src="{pic}"/>
-                    <p>{info}<br/></p>
-                </div>
-                {/planets}
+        <div class="row">
+            {planets}
+            <div class="span4">
+                <h2>{planet}</h2>
+                <img src="{pic}"/>
+                <p>{info}<br/></p>
             </div>
+            {/planets}
         </div>
     </div>
+</div>
