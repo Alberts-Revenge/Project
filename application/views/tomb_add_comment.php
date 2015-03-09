9838e38e2f62f7c7c1473d3a42e7f162d86df6a0
A00770012 <A00770012bcit@gmail.com>
2015-03-08 18:54:53 -0700

    added the tinymec rich text editor package Signed-off-by:A00770012 <A00770012bcit@gmail.com>

--- application/views/tomb_add_comment.php
+++ application/views/tomb_add_comment.php
@@ -13,11 +13,21 @@
         <td>{description}</td>
     </tr>
 </table> 
-<div class="row">
-    <!--error message formating-->
+<!--<div class="row">
+    error message formating
     <div class="errors">{message}</div>
     <form action="/tombs/confirm/{tombid}" method="post">
         {fcomment}
         {fsubmit}
+    </form>
+</div>-->
+
+<div class="row">
+    <form role="form" id="frmArticle" class="form" action="/tombs/confirm/{tombid}" method="POST">
+        <lablel>Title</lablel><p>
+        <input type="text" name="title" class="form-control"></p>   
+        <lablel>Content</lablel><p>
+        <textarea id="article" name="article" rows="8" class="form-control"></textarea></p>
+        <input class="btn btn-info btn-block" type="submit" value="Add new article" name="submit">
     </form>
 </div>
